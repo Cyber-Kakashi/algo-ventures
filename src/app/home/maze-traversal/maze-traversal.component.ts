bb5fa28832c9751debde15b032dd008962c7aa5f
Vishvesh Choudhary <vishvesh.choudhary@sarvaha.com>
2021-04-22 19:09:19 +0530

    the grid is changed to 2d array insted of 1d array, it was a mistake to use 1d array

--- src/app/home/maze-traversal/maze-traversal.component.ts
+++ src/app/home/maze-traversal/maze-traversal.component.ts
@@ -4,6 +4,15 @@ export interface Tile {
   color: string;
   cols: number;
   rows: number;
+  x: number;
+  y: number;
+}
+
+export enum Color {
+  white = 'white',
+  gray = 'gray',
+  green = 'green',
+  red = 'darkred'
 }
 @Component({
   selector: 'app-maze-traversal',
@@ -11,11 +20,12 @@ export interface Tile {
   styleUrls: ['./maze-traversal.component.scss']
 })
 export class MazeTraversalComponent implements OnInit {
-  columns: number = 50;
-  rows: number = 35;
-  tiles: Tile[] = [];
-  startPoint: Tile;
-  endPoint: Tile;
+  columns = 35;
+  rows = 70;
+  tiles: Array<Array<Tile>> = [];
+  list = [];
+  startTile: Tile;
+  endTile: Tile;
   constructor() { }
 
   ngOnInit(): void {
@@ -23,13 +33,45 @@ export class MazeTraversalComponent implements OnInit {
     let j = 0;
     for (i = 0; i < this.columns; i++) {
       for (j = 0; j < this.rows; j++) {
-        this.tiles.push({
+        this.list.push({
           cols: 1,
           rows: 1,
-          color: 'gray'
+          color: Color.gray,
+          y: i,
+          x: j
         });
+      }
+      this.tiles.push(this.list);
+      this.list = [];
+    }
+  }
+
+  seTtartTile(tile: Tile): void {
+    console.log(tile.x, tile.y);
+    if (this.startTile?.x === tile.x && this.startTile?.y === tile.y) {
+    } else {
+      if (tile.color === Color.gray) {
+        this.startTile ? this.startTile.color = Color.gray : console.log();
+        tile.color = Color.green;
+        this.startTile = tile;
       }
     }
   }
 
+  setEndTile(tile: Tile, event): void {
+    console.log('tate');
+    event.preventDefault();
+    if (this.endTile?.x === tile.x && this.endTile?.y === tile.y) {
+    } else {
+      if (tile.color === Color.gray) {
+        this.endTile ? this.endTile.color = Color.gray : console.log();
+        tile.color = Color.red;
+        this.endTile = tile;
+      }
+    }
+  }
+
+  startGame(): void {
+    console.log(this.tiles[this.startTile.y][this.startTile.x]);
+  }
 }
